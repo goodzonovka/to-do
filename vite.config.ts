ba9e1a737948ee4b4278668d337603f5e52497e9
artem <goodzonovka@gmail.com>
2025-02-20 05:33:43 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-})
+    server: {
+        host: process.env.HOST || '127.0.0.1', // Хост должен быть 127.0.0.1
+        port: process.env.PORT ? Number(process.env.PORT) : 3000, // Порт 3000
+    },
+});
