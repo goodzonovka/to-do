59da45e30988c1a6814e6880328aa2675fff278e
artem <goodzonovka@gmail.com>
2025-02-20 05:46:10 +0200

    fix 6

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,9 @@ export default defineConfig({
         },
     },
     server: {
-        host: process.env.HOST || '127.0.0.1', // Хост должен быть 127.0.0.1
-        port: process.env.PORT ? Number(process.env.PORT) : 3000, // Порт 3000
+        host: process.env.HOST || '127.0.0.1', // Проксирование требует 127.0.0.1
+        port: process.env.PORT ? Number(process.env.PORT) : 3000, // Проксирование через порт 3000
+        strictPort: true, // Гарантирует, что сервер запускается на заданном порту
+        allowedHosts: ['testing.goodzonovka.com'], // Добавляем разрешенный хост
     },
 });
